29bab87800b5e90b61e4a75a5919136674e287a8
Harnoor Kaur <63140074+harnoor497@users.noreply.github.com>
2025-04-08 08:55:54 -0700

    Update profile image

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -294,7 +294,7 @@ const Hero = () => {
               >
                 <Box
                   component="img"
-                  src="/public/images/profile.jpg"
+                  src="/public/images/projects/profile.jpg"
                   alt="Profile"
                   sx={{
                     width: '100%',
